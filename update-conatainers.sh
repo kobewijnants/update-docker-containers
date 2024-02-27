666496ab1457820e2805d01fc13dd16c9ac03fab
kobe.wijnants <kobe.wijnants@student.kdg.be>
2024-02-27 15:16:50 +0100

    add functions

--- update-conatainers.sh
+++ update-conatainers.sh
@@ -1,14 +1,14 @@
-#!/usr/bin/env 
+#!/usr/bin/env
 
 # run as sudo
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root"
+    exit
 fi
 
 # Update all Docker containers
 function update_container {
-  docker container update --restart=unless-stopped $(sudo docker container ls -aq)
+    docker container update --restart=unless-stopped $(sudo docker container ls -aq)
 }
 
 # show the list of containers
@@ -18,10 +18,10 @@ function list_containers {
 
 # choose containers to update
 function choose_containers {
-  read -p "Choose containers to update: " containers
-    IFS=',' read -ra container_array <<< "$containers"
+    read -p "Choose containers to update: " containers
+    IFS=',' read -ra container_array <<<"$containers"
     for container in "${container_array[@]}"; do
         docker container update --restart=unless-stopped "$container"
     done
-  
+
 }
