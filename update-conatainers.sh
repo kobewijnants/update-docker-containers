87fcb2cb441dcd71074fd8fefe9f152ea45e8b9f
kobe.wijnants <kobe.wijnants@student.kdg.be>
2024-02-27 15:16:47 +0100

    Add functions to update and choose containers

--- update-conatainers.sh
+++ update-conatainers.sh
@@ -7,5 +7,21 @@ if [ "$EUID" -ne 0 ]
 fi
 
 # Update all Docker containers
-sudo docker container update --restart=unless-stopped $(sudo docker container ls -aq)x
+function update_container {
+  docker container update --restart=unless-stopped $(sudo docker container ls -aq)
+}
 
+# show the list of containers
+function list_containers {
+    docker container ls --format "{{.Names}}"
+}
+
+# choose containers to update
+function choose_containers {
+  read -p "Choose containers to update: " containers
+    IFS=',' read -ra container_array <<< "$containers"
+    for container in "${container_array[@]}"; do
+        docker container update --restart=unless-stopped "$container"
+    done
+  
+}
